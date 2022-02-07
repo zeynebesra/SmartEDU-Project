a0ef174cc38900c629466f7708700db4b304e6c7
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-02-07 20:33:05 +0300

    django eğitimi başlandı.

---
+++ smartedu_con/pages/urls.py
@@ -0,0 +1,10 @@
+
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('about/',views.about , name="about"),
+]
